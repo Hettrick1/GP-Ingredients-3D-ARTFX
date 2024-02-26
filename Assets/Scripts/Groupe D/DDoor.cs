c9f8bd6db358bf21a8a657b077562425220011ab
Evann <evann.jacquot@artfx.fr>
2024-02-26 16:57:59 +0100

    [add] finish level 1

---
+++ Assets/Scripts/Groupe D/DDoor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DDoor : Interactive
+{
+
+    public override void OnInteraction()
+    {
+        Debug.LogError("LOAD THE SECOND SCENE");
+    }
+}
